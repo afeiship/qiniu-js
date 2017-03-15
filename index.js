8b1eb17405b6267a0304babdd1122bb224560d5e
afeiship <1290657123@qq.com>
2017-03-15 15:16:00 +0800

    remove unused

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-var mOxie = require('./src/moxie');
-var plupload = require('./src/plupload');
-var Qiniu = require('./src/qiniu');
-
-function init(inGlobal){
-    inGlobal.mOxie = mOxie;
-    inGlobal.plupload = plupload;
-    inGlobal.Qiniu = Qiniu;
-}
-
-module.exports={
-    mOxie:mOxie,
-    plupload:plupload,
-    Qiniu:Qiniu,
-    QiniuInit:init
-}
+module.exports=require('./src/qiniu');
